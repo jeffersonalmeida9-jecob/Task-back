db2aae1deb13e5de1d6d01bebea4c77aac272ad5
Jeff_c <jefferson.almeida9@aluno.senai.br>
2026-08-28 23:52:14 -0300

    CRUD dos usuarios

--- taskflow-api/server.js
+++ taskflow-api/server.js
@@ -91,9 +91,52 @@ app.get('/usuarios', (req, res) => {
     res.json(usuarios);
 });
 
-//resto do CRUD usuários — dia 4
-
 // ROTA 2 — Buscar usuário por ID
+app.get('/usuarios/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const usuario = usuarios.find(t => t.id === id);
+    if (!usuario) {
+        return res.status(404).json({ erro: 'Usuário não encontrado' });
+    }
+    res.json(usuario);
+});
+
+
+//ROTA 3 — Adicionar usuário 
+app.post('/usuarios', (req, res) => {
+    const { nome, email,} = req.body;
+    const novoUsuario = {
+        id: proximoId++, 
+        nome: nome,
+        email: email || '',
+    };
+    usuarios.push(novoUsuario);
+    res.status(201).json(novoUsuario);
+});
+
+// ROTA 4 — Atualizar usuário
+app.put('/usuarios/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const { nome, email } = req.body;
+    const indice = usuarios.findIndex(t => t.id === id);
+    if (indice === -1) {
+        return res.status(404).json({ erro: 'Usuário não encontrado' });
+    }
+    const tarefaAtualizada = { id, nome, email, };
+    usuarios[indice] = usuarioAtualizado;
+    res.json(usuarioAtualizado);
+});
+
+// ROTA 5 — Deletar usuarios
+app.delete('/usuarios/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const usurio = usuarios.find(t => t.id === id);
+    if (!usuario) {
+        return res.status(404).json({ erro: 'Usuário não encontrado' });
+    }
+    usuarios = usuarios.filter(t => t.id !== id);
+    res.json({ mensagem: 'Usuario removido com sucesso', id });
+});
 
 // ROTA 6 — 404 genérico
 app.use((req, res) => {
