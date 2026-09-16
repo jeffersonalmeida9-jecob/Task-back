7e7798dd6d3aa34ab534337c44d090353bfeeb4f
alunoctg <alunoctg@fiern.local>
2026-09-16 10:59:17 -0300

    Cors: google.com para porta 5173

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,5 @@
 function corsMiddleware (req, res, next) {
-    res.setHeader('Access-Constrol-Allow-Origin', 'http://www.google.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
 
     res.setHeader('Access-Control-Allow-Methods',
         'GET, POST, PUT, DELETE, OPTIONS');
